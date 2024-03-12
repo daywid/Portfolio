fd573113dd9976b39f2290b613326893cde3b46c
Daywid <88506180+daywid@users.noreply.github.com>
2024-03-12 17:40:26 -0300

    Deleting old project

--- ts/script.ts
+++
@@ -1,9 +0,0 @@
-const a: string | number = 1;
-console.log(`a is of type ${typeof a} and its value is ${a}`);
-if (typeof a === 'number') {
-  const doubled: number = a * 2;
-  console.log(`doubled is of type ${typeof doubled} and its value is ${doubled}`);
-} else {
-  console.log('a is not a number');
-}
-
